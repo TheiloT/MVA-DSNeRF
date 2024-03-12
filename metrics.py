b978827370c3176280abb9cc077f0158202b94a8
TheiloT <theilo.terrisse@gmail.com>
2024-03-12 22:04:04 +0100

    Add last experiments configs

--- metrics.py
+++ metrics.py
@@ -40,24 +40,40 @@ def fetch_test_ground_truth(dataset_name, test_scenes_ids, factor=4):
     return np.array(ground_truth_images)
 
 
-def plot_psnr_over_training(experiment_name, dataset_name, test_scenes_ids, factor=4):
+def plot_psnr_over_training(experiment_name, dataset_name, test_scenes_ids, reference_experiment_name=None, factor=4):
     # Fetch the test generations and ground truth
     generated_scenes, iterations = fetch_test_generations(experiment_name)
+    reference_generated_scenes, reference_iterations = fetch_test_generations(reference_experiment_name)
     ground_truth_scenes = fetch_test_ground_truth(dataset_name, test_scenes_ids, factor=factor)
+    
+    max_iterations = min(iterations[-1], reference_iterations[-1])
+    generated_scenes = generated_scenes[:np.searchsorted(iterations, max_iterations)+1]
+    reference_generated_scenes = reference_generated_scenes[:np.searchsorted(reference_iterations, max_iterations)+1]
+    iterations = iterations[:np.searchsorted(iterations, max_iterations)+1]
+    reference_iterations = reference_iterations[:np.searchsorted(reference_iterations, max_iterations)+1]
     
     # Compute test PSNR over training
     psnr_means = []
     psnr_mins = []
     psnr_maxs = []
+    psnr_ref_means = []
+    psnr_ref_mins = []
+    psnr_ref_maxs = []
     for eval_nb in range(generated_scenes.shape[0]):
         psnrs_curr_eval = [psnr(gt, gen) for gt, gen in zip(ground_truth_scenes, generated_scenes[eval_nb])]
         psnr_means.append(np.mean(psnrs_curr_eval))
         psnr_mins.append(np.min(psnrs_curr_eval))
         psnr_maxs.append(np.max(psnrs_curr_eval))
+        psnrs_ref_curr_eval = [psnr(gt, gen) for gt, gen in zip(ground_truth_scenes, reference_generated_scenes[eval_nb])]
+        psnr_ref_means.append(np.mean(psnrs_ref_curr_eval))
+        psnr_ref_mins.append(np.min(psnrs_ref_curr_eval))
+        psnr_ref_maxs.append(np.max(psnrs_ref_curr_eval))
     
     # Plot the PSNR values
-    plt.plot(iterations/1000, psnr_means, color="tab:blue", label='Mean PSNR')
-    plt.fill_between(iterations/1000, np.array(psnr_mins), np.array(psnr_maxs), alpha=0.3, label='Min-Max PSNR interval')
+    plt.plot(iterations/1000, psnr_means, color="tab:blue", label='Mean PSNR (KL loss)')
+    plt.fill_between(iterations/1000, np.array(psnr_mins), np.array(psnr_maxs), alpha=0.3, label='Min-Max PSNR interval (KL loss)')
+    plt.plot(reference_iterations/1000, psnr_ref_means, color="tab:red", linestyle="--", label='Mean PSNR (MSE loss)')
+    plt.fill_between(reference_iterations/1000, np.array(psnr_ref_mins), np.array(psnr_ref_maxs), alpha=0.3, color="tab:red", label='Min-Max PSNR interval (MSE loss)')
     plt.xlabel('Iterations (in thousands)')
     plt.ylabel('Test PSNR')
     plt.title('Test PSNR over training')
@@ -69,5 +85,6 @@ def plot_psnr_over_training(experiment_name, dataset_name, test_scenes_ids, fact
 if __name__ == "__main__":
     experiment_name = "table_35v_ds_no_error_scaling"
     dataset_name = "table_35v"
+    reference_experiment_name = "table_35v_ds_mse"
     test_scenes_ids = [35, 36, 37, 38, 39]
-    plot_psnr_over_training(experiment_name, dataset_name, test_scenes_ids, factor=4)
+    plot_psnr_over_training(experiment_name, dataset_name, test_scenes_ids, reference_experiment_name, factor=4)
